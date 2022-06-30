9da2bbb57e3711016ab8837b5e96a8c35ee0dd99
Bruno Lyra <lyrasolfieri@gmail.com>
2022-06-30 11:38:50 -0300

    consulta-medicamento

--- lib/pages/consultar_medicamentos.dart
+++ lib/pages/consultar_medicamentos.dart
@@ -1,16 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:app_saude/models/medicamentos.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class ConsultaMedicamentos extends StatelessWidget {
-  const ConsultaMedicamentos({Key key}) : super(key: key);
+class ConsultaMedicamentos extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<ConsultaMedicamentos> {
+  void initState() {
+    super.initState();
+    readData();
+  }
+
+  bool isLoading = true;
+  List<String> list = [];
+
+  Future<void> readData() async {
+    var url =
+        "https://emailpasswordauthpostinho-default-rtdb.firebaseio.com/medicamentos" +
+            "data.json";
+
+    try {
+      final response = await http.get(Uri.parse(url));
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      if (extractedData == null) {
+        return;
+      }
+      extractedData.forEach((medId, medData) {
+        list.add(medData["medicamento"]);
+      });
+      setState(() {
+        isLoading = false;
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
         title: Text("Medicamentos"),
       ),
+      body: isLoading
+          ? CircularProgressIndicator()
+          : ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: list.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                    height: 50,
+                    child: Center(
+                      child: Text(
+                        list[index],
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    ));
+              }),
     );
   }
 }
